e06763dbbc0aedb3ff90b64601f2d3c44ccbc69d
houssem143 <houssem14392@gmail.com>
2022-11-21 10:00:33 +0100

    adding bootstrap

---
+++ src/components/Second.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Second = () => {
+  return (
+    <div style={{backgroundColor:"grey"}}>
+        <p>this is the first page</p>
+
+    </div>
+  )
+}
+
+export default Second
